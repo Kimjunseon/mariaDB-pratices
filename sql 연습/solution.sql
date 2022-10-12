c39892c359fea8f9cb956108a410952568291cd1
Kimjunseon <junseon0722@gmail.com>
2022-10-12 16:45:45 +0900

    t

--- sql 연습/solution.sql
+++ sql 연습/solution.sql
@@ -15,6 +15,20 @@ select first_name as "이름",
 	from employees
     order by hire_date asc;
 -- 답안:
+-- 이름				성별		입사일
+-- Przemyslawa		M		1985-01-01
+-- Arie				M		1985-01-01
+-- Krassimir		F		1985-01-01
+-- Shirish			F		1985-01-01
+-- Peternela		F		1985-01-01
+-- Tonny			F		1985-01-01
+-- DeForest			M		1985-01-01
+-- Ebru				M		1985-01-01
+-- Margareta		M		1985-01-01
+-- Isamu			F		1985-01-14
+-- 					.
+--                  .
+--                  .
 
 -- 문제3.
 -- 여직원과 남직원은 각 각 몇 명이나 있나요?
@@ -22,8 +36,7 @@ select count(*)
 	from employees
      where gender = 'm';
     -- where gender = 'f';
-    
--- 답안: 남자 - 179,973 여자 - 120,051 총원 - 300024
+-- 답안: 남자 - 179,973 여자 - 120,051 총원 - 300,024
 
 -- 문제4.
 -- 현재 근무하고 있는 직원 수는 몇 명입니까? (salaries 테이블을 사용합니다.) 
@@ -51,6 +64,15 @@ select dept_name
 	from departments
     order by length(dept_name) desc;
 -- 답안:
+-- Quality Management
+-- Customer Service
+-- Human Resources
+-- Development
+-- Production
+-- Marketing
+-- Research
+-- Finance
+-- Sales
 
 -- 문제8.
 -- 현재 급여가 120,000이상 받는 사원은 몇 명이나 있습니까?
@@ -65,18 +87,28 @@ select distinct(title)
 	from titles
     order by length(title) desc;
 -- 답안:
+-- Assistant Engineer
+-- Technique Leader
+-- Senior Engineer
+-- Senior Staff
+-- Engineer
+-- Manager
+-- Staff
 
 -- 문제10
 -- 현재 Enginner 직책의 사원은 총 몇 명입니까?
 select count(distinct(emp_no))
 	from titles
     where to_date like '9999%' and title = 'Engineer';
--- 답안: 30983명
+-- 답안: 30,983명
 
 -- 문제11
 -- 사번이 13250(Zeydy)인 지원이 직책 변경 상황을 시간순으로 출력해보세요.
-select emp_no, from_date, to_date
+select emp_no, title, from_date, to_date
 	from titles
     where emp_no = '13250';
 -- 답안:
-    
+-- emp_no	title				from_date	to_date
+-- 13250	Assistant Engineer	1988-10-28	1993-10-28
+-- 13250	Engineer			1993-10-28	1998-10-27
+-- 13250	Senior Engineer		1998-10-27	9999-01-01

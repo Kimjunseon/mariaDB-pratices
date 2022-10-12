ac37e9345a799f0ba638eecc594d0083a9e265c7
Kimjunseon <junseon0722@gmail.com>
2022-10-12 11:33:55 +0900

    t

--- sql 연습/dml-pratices.sql
+++ sql 연습/dml-pratices.sql
@@ -17,6 +17,9 @@ select
 -- 테이블 보기
 show tables;
 
+-- 테이블 삭제
+-- drop table pet;
+
 -- 테이블 만들기 (DDL)
 create table pet(
 	name varchar(20), -- 20자짜리 str
@@ -57,9 +60,54 @@ update 	pet
 
 -- 조회 1: where (row 선택 조건)
 -- Q1. 1990년 이후에 태어난 아이들은? (출력: 이름, 종, 생일)
-
 select name, species, birth from pet
 	where birth > '1990-12-31';
+
+-- select name, species, birth
+-- 	from pet
+-- 	where birth > '1990-12-31';
+
+-- Gwen과 함께 사는 아이들은? (출력: 이름, 종, 집사)
+select name, species, owner
+	from pet
+	where owner = 'Gwen';
+
+-- null 다루기 1: 현재 살아있는 아이들은?(이름, 생일, 사망일)
+select name, birth, death
+	from pet
+    where death is null; -- null 구문은 is를 사용 해야함
+    
+-- null 다루기 2: 현재 죽어있는 아이들은?(이름, 생일, 사망일)
+select name, birth, death
+	from pet
+    where death is not null;
+
+-- like 검색 1(패턴매칭): 이름이 b로 시작하는 아이들은?
+select name
+	from pet
+    where name like 'b%'; -- b로 시작하는 문자
+
+-- like 검색 2(패턴매칭): 이름이 b로 시작하며 6자인 아이들은?
+select name
+	from pet
+    where name like 'b_____'; -- 언더바(_)는 개수로 몇개 더 뽑을지 결정 ex) (b_) b로 시작하며 2글자
+
+-- order by 정렬기능
+-- 아이들을 어린 순으로 출력하세요(이름, 생일)
+select name, birth
+	 from pet
+     order by birth desc; -- 내림차순 asc 오름차순 desc
+
+-- 아이들을 많은 순으로 출력하세요(이름, 생일)
+select name, birth
+	 from pet
+     order by birth asc; -- 내림차순 asc 오름차순 desc
+
+-- 집계(통계) 함수
+select count(*) -- 칼럼의 수 확인, *은 풀스캔
+	from pet;
+
+
 
 
  

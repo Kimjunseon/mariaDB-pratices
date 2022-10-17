1a4d161a8d54947a3870f79cfb2a93fdf660bfce
Kimjunseon <junseon0722@gmail.com>
2022-10-17 22:47:40 +0900

    t

--- sql 연습/모델링 연습.sql
+++ sql 연습/모델링 연습.sql
@@ -17,6 +17,7 @@ insert
 	into member(no, member_name, phone_number, member_mail, member_password)
   values(null, '김준선', '010-1234-5678', 'junseon0722@gmail.com', password(5578));
   
+  
   show tables;
   
   select *
@@ -35,4 +36,45 @@ insert
   values(null, '영어');
   
   
+select *
+  from member;
   
+select *
+  from catagory;
+  
+select *
+  from book;
+ 
+ insert
+   into book(no, book_name)
+ values(null, '동백꽃');
+  
+ insert
+   into book(no, book_name)
+ values(null, '2차 방정식');
+ 
+ insert
+   into book(no, book_name)
+ values(null, 'toeic');
+ 
+ select *
+   from cart;
+   
+ insert
+   into cart(no, book_count, book_name_no)
+ values(null, 8, 1);
+ 
+ insert
+   into cart(no, book_count, book_name_no)
+ values(null, 5, 2);
+ 
+select *
+  from orders;
+  
+ insert
+   into orders(no, order_number, order_buyer, price_amount, address)
+ values(null, 1, 'JS', 3, 'seocho');
+ 
+ select *
+  from order_book;
+ 

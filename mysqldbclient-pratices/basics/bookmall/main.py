3ca71e7c0c5407e718332ca3bc6c1c719d779a39
Kimjunseon <junseon0722@gmail.com>
2022-10-20 17:28:58 +0900

    today

--- mysqldbclient-pratices/basics/bookmall/main.py
+++ mysqldbclient-pratices/basics/bookmall/main.py
@@ -1,21 +1,39 @@
-# 회원리스트 2명
-# 카테고리리스트 3개
-# 상품리스트 3개
-# 주문 리스트 1개
-# 주문 도서 리스트 2개
+# 회원리스트 2명 이름, 전화번호, 이메일, 비밀번호
+# 카테고리리스트 3개 ex) 소설 수필 IT
+# 상품리스트 3개 제목, 가격
+# 카트리스트 2개 도서제목, 수량, 가격
+# 주문 리스트 1개 주문번호 주문자 결제금액 배송지
+# 주문 도서 리스트 2개 도서번호 도서제목 수량
+# from models import model_member, model_catagory, model_book, 카트리스트 ,model_orders, model_order_book
+from models import model_member, model_catagory
 
-def Main():
-    print("회원 리스트")
-    run_model_member()
+def run_member():
+    results = model_member.select_model_member()
+    for index, result in enumerate(results):
+        print(f"[회원 {index + 1}] 이름: {result['member_name']} 전화번호: {result['phone_number']} 이메일: {result['member_mail']} 비밀번호: {result['member_password']}")
+
+def run_catagory():
+    results = model_catagory.select_model_catagory()
+    for index, result in enumerate(results):
+        print(f"[회원 {index + 1}] 이름: {result['member_name']} 전화번호: {result['phone_number']} 이메일: {result['member_mail']} 비밀번호: {result['member_password']}")
+
+
+
+def main():
+    print("-- 회원리스트 --")
+    run_member()
 
     print("카테고리 리스트")
-    run_model_catagory()
+    run_catagory()
+    #
+    # print("상품 리스트")
+    # run_book()
+    #
+    # print("주문 리스트")
+    # run_orders()
+    #
+    # print("주문 도서 리스트")
+    # run_order_book()
 
-    print("상품 리스트")
-    run_model_book()
-
-    print("주문 리스트")
-    run_model_orders()
-
-    print("주문 도서 리스트")
-    run_model_order_book()
+if __name__ == '__main__':
+    main()
---
+++ mysqldbclient-pratices/basics/bookmall/models/model_catagory.py
@@ -0,0 +1,20 @@
+from MySQLdb import connect
+from MySQLdb.cursors import DictCursor
+
+def connetion():
+    bookmall = connect(user='bookmall',
+                        password='bookmall',
+                        host='127.0.0.1',
+                        port=3306,
+                        db='bookmall',
+                        charset='utf8')
+    return  bookmall
+def select_model_member_catagory():
+    bookmall = connetion()
+    cursor = bookmall.cursor(DictCursor)
+    sql = "select catagory_name from catagory order by no desc"
+    cursor.execute(sql)
+    results = cursor.fetchall();
+    cursor.close()
+    bookmall.close()
+    return results

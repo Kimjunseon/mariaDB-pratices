418c1ad179eea1086c782b6521b35f38e3950dc0
Kimjunseon <junseon0722@gmail.com>
2022-10-21 12:56:34 +0900

    t

--- sql 연습/bookmall.sql
+++ sql 연습/bookmall.sql
@@ -2,21 +2,24 @@ show tables;
 desc member;
 insert
   into member(no, member_name, phone_number, member_mail, member_password)
-values(null, '김준선', '010-8888-8888', 'junseon@gmail.com', 'helloworld123!');
+values(1, '김준선', '010-8888-8888', 'junseon@gmail.com', 'helloworld123!');
 
 insert
   into member(no, member_name, phone_number, member_mail, member_password)
-values(null, '김비트', '010-5555-5555', 'bit@gmail.com', 'bit555!');
+values(2, '김비트', '010-5555-5555', 'bit@gmail.com', 'bit555!');
 
 insert
   into member(no, member_name, phone_number, member_mail, member_password)
-values(null, '피카츄', '010-9999-9999', 'pika@gmail.com', 'chu555!');
+values(3, '피카츄', '010-9999-9999', 'pika@gmail.com', 'chu555!');
 
 select * from member;
 
 insert
   into order_book(no, member_no, book_number, book_name, book_count)
 values(null, 1, 001, 'javascript', 10);
+delete
+from order_book
+where no = 4;
 
 select * from order_book;
 
@@ -26,7 +29,7 @@ where a.no = b.member_no;
 
 delete
 	 from member
-     where no = 3;
+     where no = 6;
      select * from member;
      
 select *
@@ -50,7 +53,15 @@ select * from cart;
 
 insert
   into book(no, catagory_no, cart_no, book_name, price)
-values(null, 1, 1, "java", 23000);
+values(1, 1, 1, "java", 23000);
+
+insert
+  into book(no, catagory_no, cart_no, book_name, price)
+values(2, 2, 2, "사람사전", 12000);
+
+insert
+  into book(no, catagory_no, cart_no, book_name, price)
+values(3, 3, 3, "지금이순간", 9000);
 
 insert
   into cart(no, member_no, order_no, book_count, price)
@@ -59,6 +70,11 @@ values(null, 1, 1, "java", 23000);
 select * from cart;
 
 select * from orders;
+
+insert
+  into orders(no, order_number, price_amount, address)
+values(1, 22090101, 23000, "강원도 원주시");
+
 insert
   into orders(no, order_number, price_amount, address)
 values(2, 22090102, 12000, "서울시 서초구");
@@ -66,6 +82,14 @@ values(2, 22090102, 12000, "서울시 서초구");
 insert
   into orders(no, order_number, price_amount, address)
 values(3, 22090103, 9000, "경기도 마산");
+
+insert
+  into cart(no, member_no, orders_no, book_count, book_name_no, price)
+values(1, 1, 1, 1, 001, 23000);
+
+insert
+  into cart(no, member_no, orders_no, book_count, book_name_no, price)
+values(2, 2, 2, 1, 002, 12000);
 
 insert
   into cart(no, member_no, orders_no, book_count, book_name_no, price)

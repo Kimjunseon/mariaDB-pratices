9b84a84fb03e0d4ebc5f5df3c44ab571ad663d6d
Kimjunseon <junseon0722@gmail.com>
2022-11-04 00:18:19 +0900

    완성?

---
+++ jdbc-practices/sql/javaSQLPracticeCRUD.sql
@@ -0,0 +1,16 @@
+create table dept(
+no int(11) not null auto_increment,
+name varchar(45) not null,
+primary key(no))
+;
+
+
+select *
+from emaillist;
+
+insert into emaillist
+values(no, b, c,d);
+
+delete 
+from emaillist
+where no = 10;

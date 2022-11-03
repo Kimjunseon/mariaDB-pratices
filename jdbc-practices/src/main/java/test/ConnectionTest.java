8006e0603e6afe9b54f06e481822baf6a812f882
Kimjunseon <junseon0722@gmail.com>
2022-11-03 12:08:39 +0900

    c

--- jdbc-practices/src/main/java/test/ConnectionTest.java
+++ jdbc-practices/src/main/java/test/ConnectionTest.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionTest {
-	// 
+	// //
 	public static void main(String[] args) {
 		Connection conn = null;
 		try {

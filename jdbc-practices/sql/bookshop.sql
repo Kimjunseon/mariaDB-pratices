4037b4877ad7e6831f0d9d3e202200158744e664
Kimjunseon <junseon0722@gmail.com>
2022-11-04 15:33:18 +0900

    test

---
+++ jdbc-practices/sql/bookshop.sql
@@ -0,0 +1,23 @@
+show tables;
+desc book;
+desc author;
+
+-- author insert
+insert into author values(null, '원수연');
+
+-- author select
+select * from author;
+
+-- author delate all;
+delete from author;
+
+-- book insert
+insert into book values(null, 'test', '재고있음', 3);
+
+-- book select
+select * from book;
+
+-- book delete all
+delete from book where no = 25;
+
+-- findAll
--- jdbc-practices/src/main/java/test/InsertTest02.java
+++ jdbc-practices/src/main/java/test/InsertTest02.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class SelectTest01 {
+public class InsertTest02 {
 
 	public static void main(String[] args) {
 		search("pat");
